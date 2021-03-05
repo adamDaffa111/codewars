2b0ce6cf379006552a33f7d4454c75b614206f78
adamDaffa111 <adamdaffa59@gmail.com>
2021-03-05 17:18:17 +0700

    adding the new chalenge 8kyu (psyco-test)

---
+++ 8-kyu/psyco-test/script.js
@@ -0,0 +1,30 @@
+// psyco test
+
+// task 1
+/**
+let result = 3;
+for (let i = 0; i < 7; i++) {
+  result += i + 1;
+  console.log(result)
+}
+**/
+
+// task 2
+
+/*
+for (let i = 2; i < 7; i++) {
+  console.log(i,i)
+}
+*/
+
+// task 3
+let j = 8;
+for (let i = 1; i < 7; i++) {
+  if(i%2==0){
+   j++;
+   console.log(j)
+  }else {
+   i
+   console.log(i)
+  }
+}
